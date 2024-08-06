ef442f6c48ce36d130c335cf9081a019f51b90ba
Gabriel Xis <gabrielkingsx@gmail.com>
2024-08-06 09:35:06 -0300

    Adcionado input inicial

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Digite quantas linhas e colunas da matriz: ");
+		
 		int M = sc.nextInt();
 		int N = sc.nextInt();
 		int[][] mat = new int[M][N];
